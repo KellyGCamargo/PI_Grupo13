24917e3f20f4b08f0549954d3d33a45d5dfde28b
Moacir Camargo <moacir.camargo30@gmail.com>
2022-12-11 18:13:43 -0300

    criação dos controllers renderizando páginas e formulários

---
+++ controllers/pageStaticController.js
@@ -0,0 +1,48 @@
+const pageStaticController = {
+    viewPage: (req, res) => {
+        return res.render('index');
+    },
+    about: (req, res) => {
+        return res.render('PessoaFisica/sobrenos');
+    },
+    life: (req, res) => {
+        return res.render('PessoaFisica/vida');
+    },
+    personalAccidents: (req, res) => {
+        return res.render('PessoaFisica/acidentesPessoais');
+    },
+    plannedProtection: (req, res) => {
+        return res.render('PessoaFisica/protecaoPlanejada');
+    },
+    
+    privatePension: (req, res) => {
+        return res.render('PessoaFisica/previdenciaPrivada');
+    },
+    
+    protectionAndMonitoring: (req, res) => {
+        return res.render('PessoaFisica/protecaoMonitoramento');
+    },
+    healthForPet: (req, res) => {
+        return res.render('PessoaFisica/healthForPet');
+    },
+    consortium: (req, res) => {
+        return res.render('PessoaFisica/consorcio');
+    },
+    easyCar: (req, res) => {
+        return res.render('PessoaFisica/carroFacil');
+    },
+    
+    vidaColetivo: (req, res) => {
+        return res.render('PessoaJuridica/vidaColetivo');
+    },
+    acidentesPessoaisColetivo: (req, res) => {
+        return res.render('PessoaJuridica/acidentesPessoaisColetivo');
+    },
+    saudeOdonto: (req, res) => {
+        return res.render('PessoaJuridica/saudeOdonto');
+    }
+
+
+
+}
+module.exports = pageStaticController 
